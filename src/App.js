1c7a76512f42f09d7000c0213ca195b93e3bc0f9
Senad Djerlek <senajerlek@gmail.com>
2023-01-17 19:41:48 +0100

    react-cart-redux project

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { Fragment, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import { uiActions } from "./store/ui-slice";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://project-c9a60-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending Cart Data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Error",
+          title: "Error!",
+          message: " Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {!showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
